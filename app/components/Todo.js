f0e4bc7d8af1b18918bed07b4286d19c9d6bb170
Shayan Yazdanpanah <shayanypn@gmail.com>
2017-09-13 22:53:04 +0430

    initial

---
+++ app/components/Todo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Todo extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Todo</h1>
+      </div>);
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todo from './components/Todo';
+import './styles/app.scss';
+
+ReactDOM.render(<Todo />, document.getElementById('root'));
